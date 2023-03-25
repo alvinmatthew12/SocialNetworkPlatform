a51d39b40669c43074dc189377d3f0a8df1d2d5d
dev_alvin <alvinmatthew12@gmail.com>
2023-03-25 14:58:01 +0700

    Initial Commit

---
+++ SocialNetworkPlatformUITests/SocialNetworkPlatformUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SocialNetworkPlatformUITestsLaunchTests.swift
+//  SocialNetworkPlatformUITests
+//
+//  Created by Alvin Matthew Pratama on 25/03/23.
+//
+
+import XCTest
+
+final class SocialNetworkPlatformUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
