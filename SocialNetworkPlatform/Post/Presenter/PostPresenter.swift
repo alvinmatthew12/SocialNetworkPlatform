b7cf48d51598a27e3f43f39b2f6204f72bb75a98
dev_alvin <alvinmatthew12@gmail.com>
2023-03-26 01:18:14 +0700

    sort post

--- SocialNetworkPlatform/Post/Presenter/PostPresenter.swift
+++ SocialNetworkPlatform/Post/Presenter/PostPresenter.swift
@@ -21,6 +21,7 @@ extension PostPresenter: PostInteractorToPresenterProtocol {
     func postFetched(result: Result<[PostModel], CoreDataError>) {
         switch result {
         case let .success(posts):
+            let sortedPosts = posts.sorted(by: { $0.createdAt > $1.createdAt })
             view?.showPosts(posts: posts)
         case let .failure(failure):
             view?.showError(message: failure.localizedDescription)
